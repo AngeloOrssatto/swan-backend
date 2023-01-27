38db5eeba5bdcac1bf0a00af46b3c4bdc60ddbe6
Angelo <angelojorssatto@gmail.com>
2023-01-27 16:07:59 -0300

    add config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ActiveModule } from './active/active.module';
@@ -9,13 +10,16 @@ import { RegisterModule } from './register/register.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '666ricoestamos',
-      name: 'swan',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      name: process.env.DATABASE_NAME,
       synchronize: true, //only for dev
       entities: [__dirname + '**/*.entity{.ts, .js}'],
     }),
